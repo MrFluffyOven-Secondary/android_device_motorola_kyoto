d951a0d98f911e24d3c952e3be7312ea8d790b93
TheNoobDevs <thenoobdevs2024@gmail.com>
2024-09-08 17:08:54 +0100

    Omni > twrp

--- twrp_kyoto.mk
+++ twrp_kyoto.mk
@@ -9,14 +9,14 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from kyoto device
 $(call inherit-product, device/motorola/kyoto/device.mk)
 
 PRODUCT_DEVICE := kyoto
-PRODUCT_NAME := omni_kyoto
+PRODUCT_NAME := twrp_kyoto
 PRODUCT_BRAND := motorola
 PRODUCT_MODEL := motorola edge 20 lite
 PRODUCT_MANUFACTURER := motorola
